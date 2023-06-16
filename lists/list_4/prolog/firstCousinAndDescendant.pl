ce2638cace05eaed940657f709f3b13b7006f0a9
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-06-16 14:12:13 -0300

    ADVANCEMENTS WITH LIST 4

--- lists/list_4/prolog/firstCousinAndDescendant.pl
+++ lists/list_4/prolog/firstCousinAndDescendant.pl
@@ -5,9 +5,13 @@ parent(esther,margaret).
 parent(herbert,margaret).
 parent(herbert,jean).
 parent(kent, bruce).
+parent(jean, sam).
+parent(sam, max).
 greatGrandParent(GGP,GGC) :- parent(GGP,GP), parent(GP,P), parent(P,GGC).
 sibling(X,Y) :- parent(P,X), parent(P, Y), not(X=Y).
 
+% a
 firstCousin(X, Y) :- sibling(M, N), parent(M, X), parent(N, Y), not(X=Y), not(sibling(X, Y)).
 
-descendant(X, Y) :- parent(Y, X); (descendant(X, K), parent(Y, K)).
+% b
+descendant(X,Y) :- parent(Y, X); (parent(Y, K), descendant(X, K)).
