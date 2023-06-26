8066dc674aeaef54413e1182c39fa53c44cb1403
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-06-26 00:19:00 -0300

    medium is harder then i thought

--- content/prolog/p99/medium/p07.pl
+++ content/prolog/p99/medium/p07.pl
@@ -1,4 +1,3 @@
+my_flatten(L, [L]) :-  \+ is_list(L).
 my_flatten([], []).
-my_flatten(L, X) :- my_flatten_aux(L, X, []).
-my_flatten_aux([H|T], L2, ACC) :- (is_list(H), append(ACC, H, K), subtract(H, L2, N), my_flatten_aux(T, N, K)) ;
-    (not(is_list(H)), append(ACC, [H], K), select(H, L2, M), my_flatten_aux(T, M, K)).
+my_flatten([H|T], X) :- my_flatten(H, Y), my_flatten(T, Ys), append(Y, Ys, X).
